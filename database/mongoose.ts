ea07a719ccceec4d9c4dead1bd4a67543e8ab386
Shubham Bhardwaj <bhardwa7@sheridancollege.ca>
2026-01-20 12:58:50 -0500

    Implemented Database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+    if (!MONGO_URI) throw new Error('Please define the MONGO_URI environment variable inside .env.local');
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGO_URI, { bufferCommands: false });
+    }
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+    console.log(`Connected to database${process.env.NODE_ENV} - ${MONGO_URI}`);
+}
